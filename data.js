ae4e2dfaf9eb1689f855cd48d3fa6db15b8e4d3f
GabrielEtolo <145966825+GabrielEtolo@users.noreply.github.com>
2023-09-25 12:34:21 +0200

    1 binaries uploaded

--- data.js
+++ data.js
@@ -1,74 +1,37 @@
 export const data = [
   {
-    id : 0,
+    id: 0,
     titre: "Découvrir l'Art Nouveau : Histoire et Caractéristiques Principales",
-    image:"/img/1.png",
-    contenu:"This comprehensive article takes readers on a journey through the history and defining characteristics of Art Nouveau. It begins with the movement's origins in the late 19th century, primarily in Europe, and traces its evolution into a prominent artistic and architectural style. The article discusses the distinctive features of Art Nouveau, such as flowing organic lines, floral motifs, and a rejection of historical revivalism. It showcases the works of iconic artists like Alphonse Mucha, Gustav Klimt, and René Lalique, providing insights into their contributions to the movement.",
-
+    image: '/img/1.png',
+    contenu:
+      "This comprehensive article takes readers on a journey through the history and defining characteristics of Art Nouveau. It begins with the movement's origins in the late 19th century, primarily in Europe, and traces its evolution into a prominent artistic and architectural style. The article discusses the distinctive features of Art Nouveau, such as flowing organic lines, floral motifs, and a rejection of historical revivalism. It showcases the works of iconic artists like Alphonse Mucha, Gustav Klimt, and René Lalique, providing insights into their contributions to the movement.",
   },
   {
-    id : 1,
-    titre: "Art Nouveau in Architecture: Elegant and Whimsical Buildings",
-    image:"/img/2.png",
-    contenu:"This article offers a captivating exploration of how Art Nouveau left its indelible mark on architectural design around the world. It highlights famous buildings that exemplify the style, including Antoni Gaudí's Casa Batlló in Barcelona and Victor Horta's Hôtel Tassel in Brussels. Readers will gain a deeper understanding of the unique architectural elements, such as curvilinear facades, stained glass windows, and ornate ironwork, that defined Art Nouveau structures.",
+    id: 1,
+    titre: 'Art Nouveau in Architecture: Elegant and Whimsical Buildings',
+    image: '/img/2.png',
+    contenu:
+      "This article offers a captivating exploration of how Art Nouveau left its indelible mark on architectural design around the world. It highlights famous buildings that exemplify the style, including Antoni Gaudí's Casa Batlló in Barcelona and Victor Horta's Hôtel Tassel in Brussels. Readers will gain a deeper understanding of the unique architectural elements, such as curvilinear facades, stained glass windows, and ornate ironwork, that defined Art Nouveau structures.",
   },
   {
-    id : 2,
-    titre: "Art Nouveau in Decorative Arts: Refinement in the Details",
-    image:"/img/3.png",
-    contenu:"Delve into the world of Art Nouveau's influence on decorative arts in this in-depth article. It explores how the movement impacted various forms of decorative expression, from intricately designed furniture to exquisite jewelry, stunning glassware, and delicate ceramics. The article showcases exceptional pieces from the era, providing historical context and highlighting the craftsmanship that went into creating them.",},
-  {
-    id : 3,
-    titre: "Women and Art Nouveau: Femme Fatale and the New Woman",
-    image:"/img/4.png",
-    contenu:"In this insightful article, we examine the fascinating role of women in the Art Nouveau movement. It goes beyond the traditional narrative of women as muses and explores their active participation as artists, designers, and advocates for social change. The article discusses the representation of the Femme Fatale and the New Woman in Art Nouveau artworks, shedding light on how these images reflected evolving societal norms and expectations.",},
-  {
-    id : 3,
-    titre: "Art Nouveau in the Contemporary World: Influence and Reinvention",
-    image:"/img/5.png",
-    contenu:"This forward-looking article explores how Art Nouveau continues to shape and inspire contemporary art and design. It introduces readers to modern-day artists, architects, and designers who draw inspiration from the movement's aesthetics. The article showcases examples of contemporary works that pay homage to Art Nouveau while pushing the boundaries of creativity and innovation in the 21st century.",},
-  {
-    id: 6,
-    prenom: 'Franck',
-    nom: 'Moreau',
-    date_de_naissance: '1982-02-15',
-    avatar: '/img/6.png',
-    numero_de_telephone: '+33 6 33 55 99 77',
-    adresse_email: 'franck.moreau@example.com',
-    description:
-      'Champion de cache-cache professionnel et amateur de sauts en parachute depuis les arbres.',
+    id: 2,
+    titre: 'Art Nouveau in Decorative Arts: Refinement in the Details',
+    image: '/img/3.png',
+    contenu:
+      "Delve into the world of Art Nouveau's influence on decorative arts in this in-depth article. It explores how the movement impacted various forms of decorative expression, from intricately designed furniture to exquisite jewelry, stunning glassware, and delicate ceramics. The article showcases exceptional pieces from the era, providing historical context and highlighting the craftsmanship that went into creating them.",
   },
   {
-    id: 7,
-    prenom: 'Géraldine',
-    nom: 'Richard',
-    date_de_naissance: '1991-12-08',
-    avatar: '/img/7.png',
-    numero_de_telephone: '+33 6 11 22 33 44',
-    adresse_email: 'geraldine.richard@example.com',
-    description:
-      "Créatrice de chapeaux pour chats et spécialiste en imitations d'animaux de la ferme.",
+    id: 4,
+    titre: 'Women and Art Nouveau: Femme Fatale and the New Woman',
+    image: '/img/4.png',
+    contenu:
+      'In this insightful article, we examine the fascinating role of women in the Art Nouveau movement. It goes beyond the traditional narrative of women as muses and explores their active participation as artists, designers, and advocates for social change. The article discusses the representation of the Femme Fatale and the New Woman in Art Nouveau artworks, shedding light on how these images reflected evolving societal norms and expectations.',
   },
   {
-    id: 8,
-    prenom: 'Hugo',
-    nom: 'Poirier',
-    date_de_naissance: '1987-04-22',
-    avatar: '/img/8.png',
-    numero_de_telephone: '+33 6 66 77 88 99',
-    adresse_email: 'hugo.poirier@example.com',
-    description:
-      'Chercheur de trésors sous-marins et champion du monde de manger des pizzas en moins de 5 minutes.',
-  },
-  {
-    id: 9,
-    prenom: 'Isabelle',
-    nom: 'Roux',
-    date_de_naissance: '1993-08-07',
-    avatar: '/img/9.png',
-    numero_de_telephone: '+33 6 44 33 22 11',
-    adresse_email: 'isabelle.roux@example.com',
-    description:
-      'Spécialiste en mémorisation de numéros de téléphone aléatoires et créatrice de sculptures en papier toilette.',
+    id: 5,
+    titre: 'Art Nouveau in the Contemporary World: Influence and Reinvention',
+    image: '/img/5.png',
+    contenu:
+      "This forward-looking article explores how Art Nouveau continues to shape and inspire contemporary art and design. It introduces readers to modern-day artists, architects, and designers who draw inspiration from the movement's aesthetics. The article showcases examples of contemporary works that pay homage to Art Nouveau while pushing the boundaries of creativity and innovation in the 21st century.",
   },
 ];
