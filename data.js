e5556174975bbb0b36e723ce91267db04e8bc64e
GabrielEtolo <145966825+GabrielEtolo@users.noreply.github.com>
2023-09-25 12:30:46 +0200

    1 binaries uploaded

--- data.js
+++ data.js
@@ -16,18 +16,12 @@ export const data = [
     id : 2,
     titre: "Art Nouveau in Decorative Arts: Refinement in the Details",
     image:"/img/3.png",
-    contenu:"This article offers a captivating exploration of how Art Nouveau left its indelible mark on architectural design around the world. It highlights famous buildings that exemplify the style, including Antoni Gaudí's Casa Batlló in Barcelona and Victor Horta's Hôtel Tassel in Brussels. Readers will gain a deeper understanding of the unique architectural elements, such as curvilinear facades, stained glass windows, and ornate ironwork, that defined Art Nouveau structures.",
+    contenu:"Delve into the world of Art Nouveau's influence on decorative arts in this in-depth article. It explores how the movement impacted various forms of decorative expression, from intricately designed furniture to exquisite jewelry, stunning glassware, and delicate ceramics. The article showcases exceptional pieces from the era, providing historical context and highlighting the craftsmanship that went into creating them.",},
   {
-    id: 4,
-    prenom: 'David',
-    nom: 'Girard',
-    date_de_naissance: '1979-05-03',
-    avatar: '/img/4.png',
-    numero_de_telephone: '+33 6 44 88 99 00',
-    adresse_email: 'david.girard@example.com',
-    description:
-      'Inventeur de danses bizarres et détenteur du record du monde de manger des piments.',
-  },
+    id : 3,
+    titre: "Women and Art Nouveau: Femme Fatale and the New Woman",
+    image:"/img/4.png",
+    contenu:"In this insightful article, we examine the fascinating role of women in the Art Nouveau movement. It goes beyond the traditional narrative of women as muses and explores their active participation as artists, designers, and advocates for social change. The article discusses the representation of the Femme Fatale and the New Woman in Art Nouveau artworks, shedding light on how these images reflected evolving societal norms and expectations.",},
   {
     id: 5,
     prenom: 'Eva',
